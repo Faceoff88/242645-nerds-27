3f28039ad25019da3f2ae2f1f5cf38667cae17d1
Denis <feizov88@gmail.com>
2020-02-01 15:20:11 +0300

    Исправлен баг с изменением положения маркера при смене масштаба карты

--- js/yamap-custom.js
+++ js/yamap-custom.js
@@ -1,30 +1,44 @@
-ymaps.ready(init); // карта соберется после загрузки скрипта и элементов
-var myMap; // заглобалим переменную карты чтобы можно было ею вертеть из любого места
-function init() {
-  // функция - собиралка карты и фигни
-  myMap = new ymaps.Map("map", {
-    // создаем и присваиваем глобальной переменной карту и суем её в див с id="map"
-    center: [59.939199, 30.321605], // ну тут центр
-    behaviors: ["default", "scrollZoom"], // скроллинг колесом
-    zoom: 17, // тут масштаб
-    controls: []
-  });
+ymaps.ready(function() {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [59.939199, 30.321605],
+        zoom: 17,
+        controls: []
+      },
+      {
+        searchControlProvider: "yandex#search"
+      }
+    ),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemarkWithContent = new ymaps.Placemark(
+      [59.938631, 30.323055],
+      {
+        hintContent: "Собственный значок метки с контентом",
+        balloonContent:
+          "Nёrds design studio<br>Мир никогда не будет прежним после того как увидит Ваш сайт!",
+        iconContent: "12"
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#imageWithContent",
+        // Своё изображение иконки метки.
+        iconImageHref: "img/map-marker.png",
+        // Размеры метки.
+        iconImageSize: [231, 190],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-50, -190],
+        // Смещение слоя с содержимым относительно слоя с картинкой.
+        //iconContentOffset: [15, 15],
+        // Макет содержимого.
+        iconContentLayout: MyIconContentLayout
+      }
+    );
 
-  /* Создаем кастомные метки */
-  myPlacemark0 = new ymaps.Placemark(
-    [59.938631, 30.323055],
-    {
-      // Создаем метку с такими координатами и суем в переменную
-      balloonContent:
-        '<div class="ballon"><span>Nёrds design studio</span><br/><p>Мир никогда не будет прежним после того как увидит Ваш сайт!</p></div>' // сдесь содержимое балуна в формате html, все стили в css
-    },
-    {
-      iconLayout: "default#image",
-      iconImageHref: "img/map-marker.png",
-      iconImageSize: [231, 190],
-      iconImageOffset: [-40, -220]
-    }
-  );
-  /* Добавляем */
-  myMap.geoObjects.add(myPlacemark0);
-}
+  myMap.geoObjects.add(myPlacemarkWithContent);
+});
