a2a46d9f3053237fea20a65126fed75c023d1874
Denis <feizov88@gmail.com>
2020-02-01 15:15:01 +0300

    Revert "Исправлен баг с изменением размера маркера при смене масштаба карты"
    This reverts commit 90adcfa9894c2d8137f9b8142ef58bc38b4f67d5.

--- js/yamap-custom.js
+++ js/yamap-custom.js
@@ -1,4 +1,4 @@
-/*ymaps.ready(init); // карта соберется после загрузки скрипта и элементов
+ymaps.ready(init); // карта соберется после загрузки скрипта и элементов
 var myMap; // заглобалим переменную карты чтобы можно было ею вертеть из любого места
 function init() {
   // функция - собиралка карты и фигни
@@ -11,7 +11,7 @@ function init() {
   });
 
   /* Создаем кастомные метки */
-/* myPlacemark0 = new ymaps.Placemark(
+  myPlacemark0 = new ymaps.Placemark(
     [59.938631, 30.323055],
     {
       // Создаем метку с такими координатами и суем в переменную
@@ -26,57 +26,5 @@ function init() {
     }
   );
   /* Добавляем */
-/*myMap.geoObjects.add(myPlacemark0);
-
-  /* Фикс кривого выравнивания кастомных балунов */
-/*myMap.geoObjects.events.add(["balloonopen"], function(e) {
-    var geoObject = e.get("target");
-    myMap.panTo(geoObject.geometry.getCoordinates(), {
-      delay: 0
-    });
-  });
-}*/
-
-ymaps.ready(function() {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [59.939199, 30.321605],
-        zoom: 17,
-        controls: []
-      },
-      {
-        searchControlProvider: "yandex#search"
-      }
-    ),
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemarkWithContent = new ymaps.Placemark(
-      [59.938631, 30.323055],
-      {
-        hintContent: "Собственный значок метки с контентом",
-        balloonContent: "А эта — новогодняя",
-        iconContent: "12"
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#imageWithContent",
-        // Своё изображение иконки метки.
-        iconImageHref: "img/map-marker.png",
-        // Размеры метки.
-        iconImageSize: [231, 190],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-50, -190],
-        // Смещение слоя с содержимым относительно слоя с картинкой.
-        //iconContentOffset: [15, 15],
-        // Макет содержимого.
-        iconContentLayout: MyIconContentLayout
-      }
-    );
-
-  myMap.geoObjects.add(myPlacemarkWithContent);
-});
+  myMap.geoObjects.add(myPlacemark0);
+}
