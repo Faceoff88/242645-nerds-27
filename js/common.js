9285715297d46b22df5259e292662f72fdc81f7f
Denis <feizov88@gmail.com>
2020-02-11 12:35:40 +0300

    Подключена работа слайдера и его кнопки.

--- js/common.js
+++ js/common.js
@@ -8,3 +8,37 @@ openBtn.addEventListener("click", event => {
 closeBtn.addEventListener("click", event => {
   document.querySelector(".feedback-container").classList.remove("form-open");
 });
+
+var sliderBtnOne = document.getElementById("slideOne");
+var sliderBtnTwo = document.getElementById("slideTwo");
+var sliderBtnThree = document.getElementById("slideThree");
+
+sliderBtnOne.addEventListener("click", event => {
+  var posOne = document.getElementsByClassName("feature-slider-item");
+  for (var i = 0; i < posOne.length; i++) {
+    posOne[i].style.left = "0";
+  }
+  sliderBtnThree.classList.remove("active-slider-button");
+  sliderBtnTwo.classList.remove("active-slider-button");
+  sliderBtnOne.classList.add("active-slider-button");
+});
+
+sliderBtnTwo.addEventListener("click", event => {
+  var posOne = document.getElementsByClassName("feature-slider-item");
+  for (var i = 0; i < posOne.length; i++) {
+    posOne[i].style.left = "-1140px";
+  }
+  sliderBtnOne.classList.remove("active-slider-button");
+  sliderBtnThree.classList.remove("active-slider-button");
+  sliderBtnTwo.classList.add("active-slider-button");
+});
+
+sliderBtnThree.addEventListener("click", event => {
+  var posOne = document.getElementsByClassName("feature-slider-item");
+  for (var i = 0; i < posOne.length; i++) {
+    posOne[i].style.left = "-2280px";
+  }
+  sliderBtnTwo.classList.remove("active-slider-button");
+  sliderBtnOne.classList.remove("active-slider-button");
+  sliderBtnThree.classList.add("active-slider-button");
+});
