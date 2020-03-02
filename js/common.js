b127c35235f63a428002484a7954cc8f58e0a424
Denis <feizov88@gmail.com>
2020-03-02 16:59:18 +0300

    Исправленны ошибки и добавлена анимация формы

--- js/common.js
+++ js/common.js
@@ -3,11 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
   var closeBtn = document.querySelector(".feedback-close");
 
   openBtn.addEventListener("click", event => {
+    event.preventDefault();
     document.querySelector(".feedback-container").classList.add("form-open");
+    function ever() {
+      document
+        .querySelector(".feedback-container")
+        .classList.remove("form-open");
+    }
+    setTimeout(ever, 900);
+    document.querySelector(".feedback-container").classList.add("form-ever");
   });
 
   closeBtn.addEventListener("click", event => {
-    document.querySelector(".feedback-container").classList.remove("form-open");
+    document.querySelector(".feedback-container").classList.remove("form-ever");
   });
 
   var sliderBtnOne = document.getElementById("slideOne");
@@ -37,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
   sliderBtnThree.addEventListener("click", event => {
     var posOne = document.getElementsByClassName("feature-slider-item");
     for (var i = 0; i < posOne.length; i++) {
-      posOne[i].style.left = "-2280px";
+      posOne[i].style.left = "-2283px";
     }
     sliderBtnTwo.classList.remove("active-slider-button");
     sliderBtnOne.classList.remove("active-slider-button");
@@ -45,12 +53,36 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   var name = document.querySelector("[name=name]");
-  var email = document.querySelector("[email=email]");
-  var form = document.querySelector("form");
+  var email = document.querySelector("[name=email]");
+  var text = document.querySelector("[name=text]");
+  var form = document.querySelector(".feedback");
 
   form.addEventListener("submit", function(evt) {
-    if (!name.value || !email.value) {
+    if (!name.value || !email.value || !text.value.trim()) {
       evt.preventDefault();
+      document.querySelector(".feedback-container").classList.add("inv-form");
+      if (!name.value) {
+        name.classList.add("invalid");
+      } else {
+        name.classList.remove("invalid");
+      }
+      if (!email.value) {
+        email.classList.add("invalid");
+      } else {
+        email.classList.remove("invalid");
+      }
+      if (!text.value.trim()) {
+        text.classList.add("invalid");
+      } else {
+        text.classList.remove("invalid");
+      }
     }
+
+    function del() {
+      document
+        .querySelector(".feedback-container")
+        .classList.remove("inv-form");
+    }
+    setTimeout(del, 1000);
   });
 });
